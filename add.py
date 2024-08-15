72499ec5f10c60e21bc32333405ada2aa16cef29
sachinmagdum1929 <sachinmagdum1929@gmail.com>
2024-08-15 18:31:39 +0530

    code for multiplication

--- add.py
+++ add.py
@@ -2,7 +2,15 @@ def addition(num1, num2):
     ans = num1 + num2
     return ans
 
+def mul(num1, num2):
+    ans = num1 * num2
+    return ans
+
 num1 = 100
 num2 = 200
 ans = addition(num1,num2)
 print(f"Addition of {num1} and {num2} is {ans}")
+
+
+ans = mul(num1,num2)
+print(f"Multiplication of {num1} and {num2} is {ans}")
